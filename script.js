6487220a483eb56382b9402fc51b2be997e331ca
BuildTools <unconfigured@null.spigotmc.org>
2023-10-03 21:55:05 +0900

    계산기

---
+++ script.js
@@ -0,0 +1,43 @@
+let display = document.getElementById("display");
+let inputField = document.getElementById("display");
+
+function appendToDisplay(value) {
+    inputField.value += value;
+}
+
+function clearDisplay() {
+    inputField.value = "";
+}
+
+function calculateResult() {
+    try {
+        inputField.value = eval(inputField.value);
+    } catch (error) {
+        inputField.value = "Error";
+    }
+}
+
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        let lightEffect = document.createElement("div");
+        lightEffect.className = "light-effect";
+        button.appendChild(lightEffect);
+
+        setTimeout(() => {
+            button.removeChild(lightEffect); // 빛 효과 제거
+        }, 300);
+
+        button.style.opacity = "0.7"; // 클릭 시 버튼을 반투명하게 만듭니다.
+    });
+
+    button.addEventListener("mouseleave", () => {
+        button.style.opacity = "1"; // 버튼을 다시 불투명하게 만듭니다.
+    });
+});
+
+inputField.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        calculateResult();
+    }
+});
